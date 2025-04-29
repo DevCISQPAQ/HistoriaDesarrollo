325ea9f8b66a1826707292d964879594b06391a2
Alexis <it@cumbresqueretaro.com>
2025-04-29 08:12:49 -0600

    Se agrego iconos de FB y instagram

--- resources/views/historias/preescolar/seccion2.blade.php
+++ resources/views/historias/preescolar/seccion2.blade.php
@@ -22,7 +22,7 @@ $nombre_estudiante = session('nombre_estudiante')
     </div>
 
     <form action="{{ route('preescolar.seccion2.guardar') }}" method="POST" class="p-6">
-    <!-- <form action="#" method="POST"> -->
+        <!-- <form action="#" method="POST"> -->
         @csrf
 
         <!-- Datos del Padre (manteniendo tus campos originales) -->
@@ -61,13 +61,35 @@ $nombre_estudiante = session('nombre_estudiante')
                 </div>
 
                 <div>
-                    <label for="redessoc_padre" class="block text-sm font-medium text-gray-700">Redes sociales</label>
-                    <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
-                        id="redessoc_padre" name="redessoc_padre" placeholder="Facebook, Instagram">
-                </div>
+                    <label for="redessoc_padre" class="block text-sm font-medium text-gray-700 ">Redes sociales</label>
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-1">
+                        <div class="flex">
+                            <div class="flex-none pt-2">
+                                <span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#1877f2]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                        <path
+                                            d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                                    </svg>
+                                </span>
+                            </div>
+                            <input type="text" class="flex-initial  w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
+                                id="redessoc_padre" name="redessoc_padre[]" placeholder="Facebook">
+                        </div>
 
-                <!-- Manteniendo todos los campos originales del padre -->
-                <!-- ... otros campos del padre ... -->
+                        <div class="flex">
+                            <div class="flex-none pt-2">
+                                <span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#c13584]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                        <path
+                                            d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                                    </svg>
+                                </span>
+                            </div>
+                            <input type="text" class="flex-initial  w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
+                                id="redessoc_padre" name="redessoc_padre[]" placeholder="Instagram">
+                        </div>
+                    </div>
+                </div>
 
                 <!-- Diestro/Zurdo -->
                 <div class="md:col-span-2">
@@ -90,15 +112,12 @@ $nombre_estudiante = session('nombre_estudiante')
         <div class="mb-8 border border-gray-200 rounded-lg p-6 m-4">
             <h3 class="text-lg font-semibold text-[#1f355e] mb-4">Datos de la Madre</h3>
 
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <div>
                     <label for="nombre_madre" class="block text-sm font-medium text-gray-700">Nombre de la Madre <span class="text-red-500">*</span></label>
                     <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
                         id="nombre_madre" name="nombre_madre" placeholder="Nombre(s) y apellidos" required>
                 </div>
-
-                <!-- Manteniendo todos los campos originales de la madre -->
-                <!-- ... otros campos de la madre ... -->
 
                 <div>
                     <label for="edad_madre" class="block text-sm font-medium text-gray-700">Edad <span class="text-red-500">*</span></label>
@@ -126,8 +145,33 @@ $nombre_estudiante = session('nombre_estudiante')
 
                 <div>
                     <label for="redessoc_madre" class="block text-sm font-medium text-gray-700">Redes sociales</label>
-                    <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
-                        id="redessoc_madre" name="redessoc_madre" placeholder="Facebook, Instagram">
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-1">
+                        <div class="flex">
+                            <div class="flex-none pt-2">
+                                <span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#1877f2]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                        <path
+                                            d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                                    </svg>
+                                </span>
+                            </div>
+                            <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
+                                id="redessoc_madre" name="redessoc_madre[]" placeholder="Facebook">
+                        </div>
+
+                        <div class="flex">
+                            <div class="flex-none pt-2">
+                                <span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#c13584]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                        <path
+                                            d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                                    </svg>
+                                </span>
+                            </div>
+                            <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
+                                id="redessoc_madre" name="redessoc_madre[]" placeholder="Instagram">
+                        </div>
+                    </div>
                 </div>
 
                 <!-- Diestra/Zurda -->
@@ -199,7 +243,7 @@ $nombre_estudiante = session('nombre_estudiante')
 
                         <div class="pt-4 bg-[#8caab945] rounded-xl shadow-lg overflow-hidden p-4 mt-2" id="Vuelto a casar" x-show="estcivil == $el.id" x-transition>
                             <p class="text-sm font-medium text-gray-700 pb-4">Si se trata de una familia reconstructiva(padre o madre vuelto a casar por viudez, divorcio, etc) escribir los datos de la persona (diferente al padre o a la madre de origen), con quien vive el nino(a) actualmente <span class="text-red-500">*</span></p>
-                            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 pt-1">
+                            <div class="grid grid-cols-1 md:grid-cols-3 gap-6 pt-1">
                                 <div>
                                     <label for="nombre_conyuge" class="block text-sm font-medium text-gray-700">Nombre Conyuge</label>
                                     <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
@@ -232,8 +276,33 @@ $nombre_estudiante = session('nombre_estudiante')
 
                                 <div>
                                     <label for="redessoc_conyuge" class="block text-sm font-medium text-gray-700">Redes sociales</label>
-                                    <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
-                                        id="redessoc_conyuge" name="redessoc_conyuge" placeholder="Facebook, Instagram">
+                                    <div class="grid grid-cols-1 md:grid-cols-2 gap-1">
+                                        <div class="flex">
+                                            <div class="flex-none pt-2">
+                                                <span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#1877f2]">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                                        <path
+                                                            d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                                                    </svg>
+                                                </span>
+                                            </div>
+                                            <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
+                                                id="redessoc_conyuge" name="redessoc_conyuge[]" placeholder="Facebook">
+                                        </div>
+
+                                        <div class="flex">
+                                            <div class="flex-none pt-2">
+                                                <span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#c13584]">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                                        <path
+                                                            d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                                                    </svg>
+                                                </span>
+                                            </div>
+                                            <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#1f355e] focus:border-[#1f355e] transition"
+                                                id="redessoc_conyuge" name="redessoc_conyuge[]" placeholder="Instagram">
+                                        </div>
+                                    </div>
                                 </div>
 
                                 <!-- Diestra/Zurda -->
